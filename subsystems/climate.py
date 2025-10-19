eb5f1fee845a5f3fc3b4ff4f7809eaaf11c36b51
Hanulus <chingizuraimov@gmail.com>
2025-10-19 23:13:07 +0500

    Create climate.py

---
+++ subsystems/climate.py
@@ -0,0 +1,7 @@
+# Subsystem 2: Climate Control
+class ClimateControl:
+    def set_temperature(self, temp):
+        print(f"🌡️ Temperature set to {temp}°C")
+    
+    def turn_off_heating(self):
+        print("🌡️ Heating turned OFF")
