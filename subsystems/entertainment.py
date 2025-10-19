4057844e4325d6efe49bee5d2cb4a66d56ed16f1
Hanulus <chingizuraimov@gmail.com>
2025-10-19 23:15:32 +0500

    Create entertainment.py

---
+++ subsystems/entertainment.py
@@ -0,0 +1,13 @@
+# Subsystem 4: Entertainment
+class EntertainmentSystem:
+    def turn_on_tv(self):
+        print("📺 TV turned ON")
+    
+    def turn_off_tv(self):
+        print("📺 TV turned OFF")
+    
+    def play_music(self):
+        print("🎵 Music playing")
+    
+    def stop_music(self):
+        print("🎵 Music stopped")
