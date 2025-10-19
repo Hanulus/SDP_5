0b8cc2f68cb8e463a355f18b2e2bb3320f91d77d
Hanulus <chingizuraimov@gmail.com>
2025-10-19 23:14:59 +0500

    Create security.py

---
+++ subsystems/security.py
@@ -0,0 +1,13 @@
+# Subsystem 3: Security
+class SecuritySystem:
+    def lock_doors(self):
+        print("🔒 All doors LOCKED")
+    
+    def unlock_doors(self):
+        print("🔓 All doors UNLOCKED")
+    
+    def arm_alarm(self):
+        print("🚨 Alarm system ARMED")
+    
+    def disarm_alarm(self):
+        print("🚨 Alarm system DISARMED")
