8582f15f288d0b352a4ee700cd99d495505d868f
Hanulus <chingizuraimov@gmail.com>
2025-10-19 23:11:39 +0500

    Create lighting.py

---
+++ subsystems/lighting.py
@@ -0,0 +1,10 @@
+# Subsystem 1: Lighting
+class LightingSystem:
+    def turn_on(self):
+        print("💡 Lights turned ON")
+    
+    def turn_off(self):
+        print("💡 Lights turned OFF")
+    
+    def dim_lights(self, level):
+        print(f"💡 Lights dimmed to {level}%")
